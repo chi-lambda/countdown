f3f864db596c112b49fd0b3df17646897e985ceb
chi-lambda <15052875+chi-lambda@users.noreply.github.com>
2024-01-14 11:08:42 +0100

    add subdivide and parse

--- main.hs
+++ main.hs
@@ -1,4 +1,25 @@
-module Main where
+module Main (main) where
+
+data Operation = Plus | Minus | Times | Div deriving (Eq)
+
+instance Show Operation where
+  show Plus = "+"
+  show Minus = "-"
+  show Times = "*"
+  show Div = "/"
+
+parse :: String -> [Int]
+parse = map read . words
+
+subdivide :: [Int] -> [([Int], [Int])]
+subdivide numbers =
+  let len = length numbers
+      num = 2 ^ len :: Int
+      bitsplit [] result _ = result
+      bitsplit (x : xs) (r1, r2) n | even n = bitsplit xs (x : r1, r2) (n `div` 2)
+      bitsplit (x : xs) (r1, r2) n = bitsplit xs (r1, x : r2) (n `div` 2)
+      divisions = map (bitsplit numbers ([], [])) [1 .. num - 2]
+   in divisions
 
 main :: IO ()
-main = return ()
+main = interact $ show . subdivide . parse
