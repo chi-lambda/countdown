57da107ec94e45c64995df7eb7bf2890e998071e
chi-lambda <15052875+chi-lambda@users.noreply.github.com>
2024-01-15 08:23:00 +0100

    Result type added and fewer results created

--- main.hs
+++ main.hs
@@ -2,9 +2,10 @@ module Main (main) where
 
 import Control.Applicative (liftA2)
 import Control.Monad (join)
+import Data.Foldable (find)
 import Data.List (sortOn)
-import GHC.Natural (minusNaturalMaybe)
 import Numeric.Natural (Natural)
+import System.Environment (getArgs)
 
 data Operation = Plus | Minus | Times | Div deriving (Eq, Enum)
 
@@ -20,23 +21,40 @@ instance Show Term where
   show (Term op left right) = "(" ++ show left ++ show op ++ show right ++ ")"
   show (Single i) = show i
 
+data Result = Result Term Natural deriving (Eq)
+
+instance Show Result where
+  show (Result term result) = show result ++ " = " ++ show term
+
 maybeDiv :: Natural -> Natural -> Maybe Natural
 maybeDiv _ 0 = Nothing
 maybeDiv x y =
   let (d, m) = x `divMod` y
    in if m == 0 then Just d else Nothing
 
+minusNaturalMaybe x y | x > y = Just $ x - y
+minusNaturalMaybe _ _ = Nothing
+
+times :: Natural -> Natural -> Maybe Natural
+times 1 _ = Nothing
+times _ 1 = Nothing
+times x y | x > y = Nothing
+times x y = Just $ x * y
+
+add x y | x > y = Nothing
+add x y = Just $ x + y
+
 evaluate :: Term -> Maybe Natural
 evaluate (Single i) = Just i
-evaluate (Term Plus left right) = liftA2 (+) (evaluate left) (evaluate right)
+evaluate (Term Plus left right) = join $ liftA2 add (evaluate left) (evaluate right)
 evaluate (Term Minus left right) = join $ liftA2 minusNaturalMaybe (evaluate left) (evaluate right)
-evaluate (Term Times left right) = liftA2 (*) (evaluate left) (evaluate right)
+evaluate (Term Times left right) = join $ liftA2 times (evaluate left) (evaluate right)
 evaluate (Term Div left right) = join $ liftA2 maybeDiv (evaluate left) (evaluate right)
 
 parse :: String -> (Natural, [Natural])
 parse = headTail . map read . words
   where
-    headTail (x : xs) = (x, xs)
+    headTail xs = (head xs, tail xs)
 
 subdivide :: [Natural] -> [([Natural], [Natural])]
 subdivide numbers =
@@ -50,7 +68,10 @@ subdivide numbers =
 
 terms :: [Natural] -> [Term]
 terms [x] = [Single x]
-terms xs = [Term op leftTerm rightTerm | op <- [Plus .. Div], (left, right) <- subdivide xs, leftTerm <- terms left, rightTerm <- terms right]
+terms xs = [Single i | i <- xs] ++ [Term op leftTerm rightTerm | op <- [Plus .. Div], (left, right) <- subdivide xs, leftTerm <- terms left, rightTerm <- terms right]
+
+size (Single _) = 1
+size (Term _ left right) = size left + size right
 
 toSnd :: (t -> b) -> t -> (t, b)
 toSnd f a = (a, f a)
@@ -60,15 +81,16 @@ catSndMaybes [] = []
 catSndMaybes ((_, Nothing) : xs) = catSndMaybes xs
 catSndMaybes ((a, Just fa) : xs) = (a, fa) : catSndMaybes xs
 
-solve :: Natural -> [Natural] -> [(Term, Natural)]
+solve :: Natural -> [Natural] -> Maybe Result
 solve target numbers =
   let ts = terms numbers
       filterF x | x > target = x - target <= 10
       filterF x = target - x <= 10
       sortF x | x > target = x - target
       sortF x = target - x
-      ps = sortOn (sortF . snd) $ filter (filterF . snd) $ catSndMaybes $ map (toSnd evaluate) ts
-   in take 5 ps
+      --   ps = map (uncurry Result) $ sortOn (sortF . snd) $ filter (filterF . snd) $ catSndMaybes $ map (toSnd evaluate) ts
+      ps = uncurry Result <$> find ((== target) . snd) (catSndMaybes $ map (toSnd evaluate) ts)
+   in ps
 
 main :: IO ()
 main = interact $ show . uncurry solve . parse
